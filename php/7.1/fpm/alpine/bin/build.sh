e1459298a0f87e7b12d157752ee6e276b624eaac
Voskobovich Vitaly <raficone@gmail.com>
2017-01-16 13:40:10 +0200

    Refactored build.sh

--- php/7.1/fpm/alpine/bin/build.sh
+++ php/7.1/fpm/alpine/bin/build.sh
@@ -3,12 +3,14 @@
 set -ex
 
 REPOSITORY_NAME="voskobovich/symfony3-php"
-BUILD_TAG="7.1.0-fpm-alpine"
+MAJOR_TAG="7.1.0-fpm-alpine"
+MINOR_TAGS="7.1-fpm-alpine 7-fpm-alpine fpm-alpine"
 
-docker build -t ${REPOSITORY_NAME}:${BUILD_TAG} .
+docker build -t "${REPOSITORY_NAME}:${MAJOR_TAG}" .
+docker push "${REPOSITORY_NAME}:${MAJOR_TAG}"
 
 # Tag and push image for each additional tag
-for TAG in 7.1-fpm-alpine 7-fpm-alpine fpm-alpine; do
-    docker tag "${REPOSITORY_NAME}:${BUILD_TAG}" "${REPOSITORY_NAME}:${TAG}"
+for TAG in ${MINOR_TAGS}; do
+    docker tag "${REPOSITORY_NAME}:${MAJOR_TAG}" "${REPOSITORY_NAME}:${TAG}"
     docker push "${REPOSITORY_NAME}:${TAG}"
 done
